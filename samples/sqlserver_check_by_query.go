07f50ee6e82a7bb699961f4fcda6d36a06edda3c
wataru0225 <wataru.kikuchi0225@gmail.com>
2017-10-31 09:13:23 +0900

    Add sample code for sqlserver and h2 database

---
+++ samples/sqlserver_check_by_query.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/wataru0225/go_nagios_plugins/lib/sqlserver"
+	"github.com/wataru0225/go_nagios_plugins/utils"
+)
+
+// /path/to/sqlserver_check_by_query -u username -p password -h www.example.com -i instance
+func main() {
+	database := lib.Set()
+
+	db := database.Connect()
+	if db == nil {
+		panic("failed")
+	}
+
+	execQuery := "SELECT COUNT(*) FROM [DB_NAME].[dbo].[TABLE_NAME]"
+
+	rows, err := db.Query(execQuery)
+	defer db.Close()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var count int
+	for rows.Next() {
+		if err := rows.Scan(&count); err != nil {
+			panic(err.Error())
+		}
+	}
+
+	if count < 1 {
+		msg := "CRITICAL ERROR"
+		utils.Critical(msg).Exit()
+	} else {
+		msg := "OK STATUS"
+		utils.Ok(msg).Exit()
+	}
+}
